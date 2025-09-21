3b80053b9636248d8366f5334441d5005550ddd8
张先生 <1824496534@qq.com>
2025-09-21 22:32:29 +0800

    首次初始化应用

---
+++ DouYuTV/DouYuTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DouYuTV
+//
+//  Created by DaviD on 2025/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DouYuTV/DouYuTVUITests/DouYuTVUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DouYuTVUITestsLaunchTests.swift
+//  DouYuTVUITests
+//
+//  Created by DaviD on 2025/9/21.
+//
+
+import XCTest
+
+class DouYuTVUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
