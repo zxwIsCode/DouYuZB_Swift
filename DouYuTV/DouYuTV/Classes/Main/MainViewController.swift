41bfcbdbc8888aab72034a73c5b332d2cb20b983
张先生 <1824496534@qq.com>
2025-09-24 16:30:59 +0800

    通过代码方式搭建主框架实现

---
+++ DouYuTV/DouYuTV/Classes/Main/MainViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MainViewController.swift
+//  DouYuTV
+//
+//  Created by DaviD on 2025/9/24.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //  测试个颜色
+//        let vc = UIViewController()
+//        vc.view.backgroundColor = UIColor.red
+//        addChild(vc)
+        addChildVC(storyName: "Home")
+        addChildVC(storyName: "Live")
+        addChildVC(storyName: "Follow")
+        addChildVC(storyName: "Profile")
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    private func addChildVC(storyName : String) {
+        let childVc = UIStoryboard.init(name: storyName, bundle: nil).instantiateInitialViewController()!
+        addChild(childVc)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
