8066ca2ded4ad3d20ff1578d35e45c11490d9a08
张先生 <1824496534@qq.com>
2025-10-01 18:13:11 +0800

    完成颜值Cell的自定义开发实现

--- DouYuTV/DouYuTV/Classes/Home/Controller/RecommendViewController.swift
+++ DouYuTV/DouYuTV/Classes/Home/Controller/RecommendViewController.swift
@@ -9,17 +9,19 @@ import UIKit
 
 private let kItemMargin : CGFloat = 10
 private let kItemW : CGFloat = (kScreenWidth - 3 * kItemMargin)/2
-private let kItemH : CGFloat = kItemW * 3/4
+private let kNormalItemH : CGFloat = kItemW * 3/4
+private let kPrettyItemH : CGFloat = kItemW * 4/3
 private let kHeaderViewH : CGFloat = 50
 
 private let kNormalCellID = "kNormalCellID"
+private let kPrettyCellID = "kPrettyCellID"
 private let kHeaderViewID = "kHeaderViewID"
 
 class RecommendViewController: UIViewController {
     
     private lazy var collectionView:UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: kItemW, height: kItemH)
+        layout.itemSize = CGSize(width: kItemW, height: kNormalItemH)
         layout.minimumLineSpacing = 0
         layout.minimumInteritemSpacing = kItemMargin
         // 分组的高度
@@ -30,12 +32,14 @@ class RecommendViewController: UIViewController {
         // 自定义实现Cell
         var cellNib = UINib(nibName: "CollectionNormalCell", bundle: nil)
         collectionView.register(cellNib, forCellWithReuseIdentifier: kNormalCellID)
+        collectionView.register(UINib(nibName: "CollectionPrettyCell", bundle: nil), forCellWithReuseIdentifier: kPrettyCellID)
 //        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: kNormalCellID)
         // 自定义实现HeaderView
         var nib = UINib(nibName: "CollectionHeaderView", bundle: nil)
         collectionView.register(nib, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: kHeaderViewID)
 //        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: kHeaderViewID)
         collectionView.dataSource = self
+        collectionView.delegate = self
         collectionView.backgroundColor = UIColor.white
         // 随着父控件的变化而变化
         collectionView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
@@ -62,7 +66,7 @@ extension RecommendViewController {
 
 }
 
-extension RecommendViewController : UICollectionViewDataSource {
+extension RecommendViewController : UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 12
     }
@@ -75,7 +79,14 @@ extension RecommendViewController : UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
+        let cell:UICollectionViewCell
+        if indexPath.section == 1 {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: kPrettyCellID, for: indexPath)
+        }
+        else {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
+        }
+        
 //        cell.backgroundColor = UIColor.red
         return cell
     }
@@ -84,5 +95,14 @@ extension RecommendViewController : UICollectionViewDataSource {
         let headerView = collectionView .dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: kHeaderViewID, for: indexPath)
 //        headerView.backgroundColor = UIColor.gray
         return headerView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.section == 1 {
+            return CGSize(width: kItemW, height: kPrettyItemH)
+        }
+        else {
+            return CGSize(width: kItemW, height: kNormalItemH)
+        }
     }
 }
---
+++ DouYuTV/DouYuTV/Classes/Main/View/CollectionPrettyCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionPrettyCell.swift
+//  DouYuTV
+//
+//  Created by DaviD on 2025/10/1.
+//
+
+import UIKit
+
+class CollectionPrettyCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
