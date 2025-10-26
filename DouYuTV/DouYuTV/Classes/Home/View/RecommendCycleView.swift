ea9e43b65f7940a0655606e88bd62f257fb209ee
张先生 <1824496534@qq.com>
2025-10-26 22:35:53 +0800

    无限轮播的的基础页面搭建完成

--- DouYuTV/DouYuTV/Classes/Home/View/RecommendCycleView.swift
+++ DouYuTV/DouYuTV/Classes/Home/View/RecommendCycleView.swift
@@ -7,17 +7,57 @@
 
 import UIKit
 
+private let kCycleCellId : String = "kCycleCellId"
+
 class RecommendCycleView: UIView {
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    // 该方法，当从xib中加载出来调用
     override  func awakeFromNib() {
         super.awakeFromNib()
         // 设置该控件的不随着父控件的拉伸而拉伸
        autoresizingMask = []
+        
+        // 注册cell
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: kCycleCellId)
+        
+        
+    }
+    
+    // 比awakeFromNib加载晚一些
+    override func layoutSubviews() {
+        // 设置collectionView的layout
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        layout.scrollDirection = .horizontal
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        
     }
     
 }
 
+// 快速创建RecommendCycleView的方法
 extension RecommendCycleView {
     class func recommendCycleView() -> RecommendCycleView {
         return Bundle.main.loadNibNamed("RecommendCycleView", owner: nil)?.first as! RecommendCycleView
+    }
+}
+
+extension RecommendCycleView : UICollectionViewDataSource {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kCycleCellId, for: indexPath)
+        cell.backgroundColor = indexPath.item % 2 == 0 ? UIColor.red : UIColor.blue
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
     }
 }
